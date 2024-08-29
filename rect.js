14c79af6ac66778362e3eef51a8650a50f56c68b
Tam <mthanimr24@gmail.com>
2024-08-29 20:56:43 +0100

    improving code to yield better results

--- rect.js
+++ rect.js
@@ -15,4 +15,35 @@ class Rect {
         fill(this.red, this.green, this.blue, 255);
         rect(this.x, this.y, this.width, this.height);
     }
+
+    //only return a new rect
+    mutate(){
+        let mutatedRect = new Rect(this.width,this.height,this.x,this.y,this.red,this.green,this.blue);
+
+        mutatedRect.width  += random(-10, 10);
+        mutatedRect.height += random(-10, 10);
+        mutatedRect.x      += random(-10, 10);
+        mutatedRect.y      += random(-10, 10);
+        mutatedRect.red    += random(-10, 10);
+        mutatedRect.green  += random(-10, 10);
+        mutatedRect.blue   += random(-10, 10);
+ 
+        mutatedRect.x      = constrain(mutatedRect.x, 0, img.width);
+        mutatedRect.y      = constrain(mutatedRect.y, 0, img.height);
+        mutatedRect.width  = constrain(mutatedRect.width, mutatedRect.x, img.width);
+        mutatedRect.height = constrain(mutatedRect.height, mutatedRect.y, img.height);
+        mutatedRect.red    = constrain(mutatedRect.red, 0, 255);
+        mutatedRect.green  = constrain(mutatedRect.green, 0, 255);
+        mutatedRect.blue   = constrain(mutatedRect.blue, 0, 255);
+
+        mutatedRect.width  = floor(this.width);
+        mutatedRect.height = floor(this.height);
+        mutatedRect.x      = floor(this.x);
+        mutatedRect.y      = floor(this.y);
+        mutatedRect.red    = floor(this.red);
+        mutatedRect.green  = floor(this.green);
+        mutatedRect.blue   = floor(this.blue);
+
+        return mutatedRect;
+    }
 }
--- sketch.js
+++ sketch.js
@@ -33,83 +33,148 @@ function setup() {
 
     pixelDensity(1);
 
+    background(0);
+    
+    blendMode(DIFFERENCE);
     noStroke();
 
-    let newRect = [];
-
-    for(let i = 0; i < 20; i++){
-        let x = random(0, width);
-        let y = random(0, height);
-        let w = random(x, width);
-        let h = random(y, height);
-
-        x = floor(x);
-        y = floor(y);
-        w = floor(w);
-        h = floor(h);
-
-        //get colour average said area
-        let avg = getColourAvg(x,y,w,h);
-
-        newRect.push(new Rect(w,h,x,y,avg[0],avg[1],avg[2]));
-    }
-
-    for(let i = 0; i < newRect.length; i++){
-        newRect[i].draw();
-    }
-
-    //add new rectangles
-    //create generations of the rectangle
-    //sort the generations
-    //delete all the losers
-    //mutate the winners
-    //redo like 10 times
     
-    // Load the pixels array.
+    //image(img, 25, 25, 50, 50, 25, 25, 50, 50);
+
+    image(img, 0, 0);
+
+    let score = 0;
+
     loadPixels();
-    img.loadPixels();
-
-    // Set the pixel(s) at the center of the canvas black.
-    
-    imgScore = 0;
-
     for(let i = 0; i < width; i++){
         for(let j = 0; j < height; j++){
+            let r = pixels[(i + j * width) * 4];
+            let g = pixels[(i + j * width) * 4 + 1];
+            let b = pixels[(i + j * width) * 4 + 2];
 
-            //images
-            let Ur = img.pixels[(i + j * img.width) * 4];
-            let Ug = img.pixels[(i + j * img.width) * 4 + 1];
-            let Ub = img.pixels[(i + j * img.width) * 4 + 2];
-
-            //canvas
-            let Vr = pixels[(i + j * width) * 4];
-            let Vg = pixels[(i + j * width) * 4 + 1];
-            let Vb = pixels[(i + j * width) * 4 + 2];
-
-            let r = Vr - Ur;
-            let g = Vg - Ug;
-            let b = Vb - Ub;
-
-            imgScore += r + g + b;
-
-            //pixels[(i + j * width) * 4] = r;
-            //pixels[(i + j * width) * 4 + 1] = g;
-            //pixels[(i + j * width) * 4 + 2] = b;
-            //pixels[(i + j * width) * 4 + 3] = 255;
+            score += r + g + b;
         }
     }
-
-    console.log(imgScore);
-
-    // Update the canvas.
+    
     updatePixels();
-    img.updatePixels();
 
+    console.log(score);
 }
+
+
+
+//function setup() {
+    //createCanvas(img.width, img.height, WEBGL);
+
+    //pixelDensity(1);
+
+    //noStroke();
+
+    //for(let m = 0; m < 100; m++){
+    
+    
+
+    //let newRect = [];
+    //for(let i = 0; i < 100; i++){
+        //let x = random(0, width);
+        //let y = random(0, height);
+        //let w = random(x, width);
+        //let h = random(y, height);
+
+        //x = floor(x);
+        //y = floor(y);
+        //w = floor(w);
+        //h = floor(h);
+
+        ////get colour average said area
+        //let avg = getColourAvg(x,y,w,h);
+
+        //newRect.push(new Rect(w,h,x,y,avg[0],avg[1],avg[2]));
+    //}
+
+    //img.loadPixels();
+
+    ////iterating same rect
+    //for(let h = 0; h < 100; h++){
+ 
+        //for(let i = 0; i < newRect.length; i++){
+        
+            //newRect[i].draw();
+
+            //// Load the pixels array.
+            //loadPixels();
+    
+            //imgScore = 0;
+
+            //for(let i = 0; i < width; i++){
+                //for(let j = 0; j < height; j++){
+
+                    ////images
+                    //let Ur = img.pixels[(i + j * img.width) * 4];
+                    //let Ug = img.pixels[(i + j * img.width) * 4 + 1];
+                    //let Ub = img.pixels[(i + j * img.width) * 4 + 2];
+
+                    ////canvas
+                    //let Vr = pixels[(i + j * width) * 4];
+                    //let Vg = pixels[(i + j * width) * 4 + 1];
+                    //let Vb = pixels[(i + j * width) * 4 + 2];
+
+                    //let r = Vr - Ur;
+                    //let g = Vg - Ug;
+                    //let b = Vb - Ub;
+
+                    //imgScore += r + g + b;
+
+                    ////pixels[(i + j * width) * 4] = r;
+                    ////pixels[(i + j * width) * 4 + 1] = g;
+                    ////pixels[(i + j * width) * 4 + 2] = b;
+                    ////pixels[(i + j * width) * 4 + 3] = 255;
+                //}
+            //}
+
+            //newRect[i].score = imgScore;
+            //clear();
+        //}
+
+        //newRect.sort((a,b) => {
+            //if(a > b){
+                //return 1;
+            //} else if (a == b) {
+                //return 0;
+            //}
+        
+            //return -1;
+        //});
+
+        //newRect.splice(10, newRect.length - 10);
+
+        //for(let i = 0; i < 10; i++){
+            //for(let j = 0; j < 10; j++){
+                //newRect.push(newRect[i].mutate());
+            //}
+        //}
+
+    //}
+
+        //finalArray.push(newRect[0]);
+
+    //}
+
+    //for(let i = 0; i < finalArray.length; i++){
+        //console.log(finalArray[i].score);
+    //}
+
+    //// Update the canvas.
+    //updatePixels();
+    //img.updatePixels();
+//}
+
+
+
+
 function draw() {
     //sky blue background
     //background(135, 206, 235);
-
 
 
     //image(img, 0, 0);
