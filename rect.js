999045c135dfc954d8a91116853600326e0bdacc
Tam <mthanimr24@gmail.com>
2024-08-29 23:20:43 +0100

    step one complete

--- rect.js
+++ rect.js
@@ -9,6 +9,24 @@ class Rect {
         this.green = green;
         this.blue = blue;
         this.score = 0;
+
+        this.gene = [width, height, x, y, red, green, blue];
+    }
+
+    useGene(gene){
+        if(gene.length < 7) {
+            throw new console.error("faulty gene: gene length is too short / long"); 
+        }
+
+        this.width  = gene[0]; 
+        this.height = gene[1]; 
+        this.x      = gene[2]; 
+        this.y      = gene[3]; 
+        this.red    = gene[4]; 
+        this.green  = gene[5]; 
+        this.blue   = gene[6]; 
+
+        this.gene = gene;
     }
 
     draw(){
@@ -16,34 +34,51 @@ class Rect {
         rect(this.x, this.y, this.width, this.height);
     }
 
-    //only return a new rect
-    mutate(){
-        let mutatedRect = new Rect(this.width,this.height,this.x,this.y,this.red,this.green,this.blue);
+    //the gene sharing rate is 50:50
+    crossover(rect1, rect2){
+        let childGene = [];
+        for(let i = 0; i < rect1.gene.length; i++){
+            if(random() > 0.5){
+                childGene.push(rect1.gene[i]);
+            } else {
+                childGene.push(rect2.gene[i]);
+            }
+        }
 
-        mutatedRect.width  += random(-10, 10);
-        mutatedRect.height += random(-10, 10);
-        mutatedRect.x      += random(-10, 10);
-        mutatedRect.y      += random(-10, 10);
-        mutatedRect.red    += random(-10, 10);
-        mutatedRect.green  += random(-10, 10);
-        mutatedRect.blue   += random(-10, 10);
- 
-        mutatedRect.x      = constrain(mutatedRect.x, 0, img.width);
-        mutatedRect.y      = constrain(mutatedRect.y, 0, img.height);
-        mutatedRect.width  = constrain(mutatedRect.width, mutatedRect.x, img.width);
-        mutatedRect.height = constrain(mutatedRect.height, mutatedRect.y, img.height);
-        mutatedRect.red    = constrain(mutatedRect.red, 0, 255);
-        mutatedRect.green  = constrain(mutatedRect.green, 0, 255);
-        mutatedRect.blue   = constrain(mutatedRect.blue, 0, 255);
+        return childGene;
+    }
 
-        mutatedRect.width  = floor(this.width);
-        mutatedRect.height = floor(this.height);
-        mutatedRect.x      = floor(this.x);
-        mutatedRect.y      = floor(this.y);
-        mutatedRect.red    = floor(this.red);
-        mutatedRect.green  = floor(this.green);
-        mutatedRect.blue   = floor(this.blue);
+    mutate(chance){
+        let mutGene = this.gene;
 
-        return mutatedRect;
+        for(let i = 0; i < mutGene.length; i++){
+            if(random() < chance){
+                mutGene[i] += random(-1, 1);
+            }
+        }
+
+        this.width  = mutGene[0]; 
+        this.height = mutGene[1]; 
+        this.x      = mutGene[2]; 
+        this.y      = mutGene[3]; 
+        this.red    = mutGene[4]; 
+        this.green  = mutGene[5]; 
+        this.blue   = mutGene[6]; 
+
+        this.x      = constrain(mutatedRect.x, 0, img.width);
+        this.y      = constrain(mutatedRect.y, 0, img.height);
+        this.width  = constrain(mutatedRect.width, mutatedRect.x, img.width);
+        this.height = constrain(mutatedRect.height, mutatedRect.y, img.height);
+        this.red    = constrain(mutatedRect.red, 0, 255);
+        this.green  = constrain(mutatedRect.green, 0, 255);
+        this.blue   = constrain(mutatedRect.blue, 0, 255);
+
+        this.width  = floor(this.width);
+        this.height = floor(this.height);
+        this.x      = floor(this.x);
+        this.y      = floor(this.y);
+        this.red    = floor(this.red);
+        this.green  = floor(this.green);
+        this.blue   = floor(this.blue);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,3 +1,27 @@
+/*
+
+Steps to do genetic algorithm
+
+1) Create a population of rectangles
+
+2) Calculate the fitness of the rectangles
+
+3) Reproduce / Selection
+
+    a. pick 2 parents
+    b. make a new rectangle
+        i - crossover, in which theres a chance the rectangle inherits features from one parent
+        ii - mutation, in which there's a chance one value is slightly modified
+
+4) replace the old population with the new population and repeat step 2
+
+5) optionally, start a new population, which works ontop of the previous champion rectangle
+
+
+*/
+
+const mutation = 0.01;
+
 let img;
 let finalArray = [];
 
@@ -38,144 +62,82 @@ function setup() {
     blendMode(DIFFERENCE);
     noStroke();
 
-    
-    //image(img, 25, 25, 50, 50, 25, 25, 50, 50);
+    //step 1
+    let population = [];
 
-    image(img, 0, 0);
+    for(let i = 0; i < 20; i++){
+        let x = random(0, width);
+        let y = random(0, height);
+        let w = random(x, width);
+        let h = random(y, height);
 
-    let score = 0;
+        x = floor(x);
+        y = floor(y);
+        w = floor(w);
+        h = floor(h);
 
-    loadPixels();
-    for(let i = 0; i < width; i++){
-        for(let j = 0; j < height; j++){
-            let r = pixels[(i + j * width) * 4];
-            let g = pixels[(i + j * width) * 4 + 1];
-            let b = pixels[(i + j * width) * 4 + 2];
+        //get colour average said area
+        let avg = getColourAvg(x,y,w,h);
 
-            score += r + g + b;
-        }
+        population.push(new Rect(w,h,x,y,avg[0],avg[1],avg[2]));
     }
-    
-    updatePixels();
 
-    console.log(score);
+
+    for(let i = 0; i < population.length; i++){
+
+        let score = 0;
+
+        image(img, 0, 0);
+
+        population[i].draw();
+
+        loadPixels();
+
+        for(let i = 0; i < width; i++){
+            for(let j = 0; j < height; j++){
+                let r = pixels[(i + j * width) * 4];
+                let g = pixels[(i + j * width) * 4 + 1];
+                let b = pixels[(i + j * width) * 4 + 2];
+
+                score += r + g + b;
+            }
+        }
+
+        updatePixels();
+        
+        population[i].score = score;
+    
+        clear();
+    }
+
+    population.sort((a,b) => {
+        if(a.score < b.score){
+            return 1;
+        } else if (a.score == b.score) {
+            return 0;
+        } else {
+            return -1;
+        }
+    })
+
+    for(let i = 0; i < population.length; i++){
+        console.log(population[i].score);
+    }
+
+    finalArray = population;
+
+    blendMode(BLEND);
+
+    for(let i = 0; i < finalArray.length; i++){
+        finalArray[i].draw();
+    }
 }
-
-
-
-//function setup() {
-    //createCanvas(img.width, img.height, WEBGL);
-
-    //pixelDensity(1);
-
-    //noStroke();
-
-    //for(let m = 0; m < 100; m++){
-    
-    
-
-    //let newRect = [];
-    //for(let i = 0; i < 100; i++){
-        //let x = random(0, width);
-        //let y = random(0, height);
-        //let w = random(x, width);
-        //let h = random(y, height);
-
-        //x = floor(x);
-        //y = floor(y);
-        //w = floor(w);
-        //h = floor(h);
-
-        ////get colour average said area
-        //let avg = getColourAvg(x,y,w,h);
-
-        //newRect.push(new Rect(w,h,x,y,avg[0],avg[1],avg[2]));
-    //}
-
-    //img.loadPixels();
-
-    ////iterating same rect
-    //for(let h = 0; h < 100; h++){
- 
-        //for(let i = 0; i < newRect.length; i++){
-        
-            //newRect[i].draw();
-
-            //// Load the pixels array.
-            //loadPixels();
-    
-            //imgScore = 0;
-
-            //for(let i = 0; i < width; i++){
-                //for(let j = 0; j < height; j++){
-
-                    ////images
-                    //let Ur = img.pixels[(i + j * img.width) * 4];
-                    //let Ug = img.pixels[(i + j * img.width) * 4 + 1];
-                    //let Ub = img.pixels[(i + j * img.width) * 4 + 2];
-
-                    ////canvas
-                    //let Vr = pixels[(i + j * width) * 4];
-                    //let Vg = pixels[(i + j * width) * 4 + 1];
-                    //let Vb = pixels[(i + j * width) * 4 + 2];
-
-                    //let r = Vr - Ur;
-                    //let g = Vg - Ug;
-                    //let b = Vb - Ub;
-
-                    //imgScore += r + g + b;
-
-                    ////pixels[(i + j * width) * 4] = r;
-                    ////pixels[(i + j * width) * 4 + 1] = g;
-                    ////pixels[(i + j * width) * 4 + 2] = b;
-                    ////pixels[(i + j * width) * 4 + 3] = 255;
-                //}
-            //}
-
-            //newRect[i].score = imgScore;
-            //clear();
-        //}
-
-        //newRect.sort((a,b) => {
-            //if(a > b){
-                //return 1;
-            //} else if (a == b) {
-                //return 0;
-            //}
-        
-            //return -1;
-        //});
-
-        //newRect.splice(10, newRect.length - 10);
-
-        //for(let i = 0; i < 10; i++){
-            //for(let j = 0; j < 10; j++){
-                //newRect.push(newRect[i].mutate());
-            //}
-        //}
-
-    //}
-
-        //finalArray.push(newRect[0]);
-
-    //}
-
-    //for(let i = 0; i < finalArray.length; i++){
-        //console.log(finalArray[i].score);
-    //}
-
-    //// Update the canvas.
-    //updatePixels();
-    //img.updatePixels();
-//}
-
-
-
 
 function draw() {
     //sky blue background
     //background(135, 206, 235);
-
+    
+    
 
     //image(img, 0, 0);
 }
